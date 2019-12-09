f8e9bbc6332184f39409a6baff7f25ee72637895
jlink-datev <jlink-datev@datev.de>
2019-12-09 16:12:23 +0100

    optional port use in run batch script now really works

--- run-app.bat
+++ run-app.bat
@@ -1,11 +1,5 @@
+IF "%1" equ "" (
+java -jar web/target/web-1.0-SNAPSHOT.jar --server.port=0
+) ELSE (
 java -jar web/target/web-1.0-SNAPSHOT.jar --server.port=%1
-
-REM @echo off
-REM netstat -na | find "9999"
-REM IF %ERRORLEVEL% equ 0 (
-REM set port = 0
-REM echo "Port already in use"
-REM ) else (
-REM set port = %1
-REM echo "Port not in use"
-REM)
+)
