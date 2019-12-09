cf63ec6370a7956c022388bfca8cb48a0c7f1124
jlink-datev <jlink-datev@datev.de>
2019-12-09 16:00:05 +0100

    optional port param for run-app batch script added

--- run-app.bat
+++ run-app.bat
@@ -1,1 +1,11 @@
-java -jar web/target/web-1.0-SNAPSHOT.jar
+java -jar web/target/web-1.0-SNAPSHOT.jar --server.port=%1
+
+REM @echo off
+REM netstat -na | find "9999"
+REM IF %ERRORLEVEL% equ 0 (
+REM set port = 0
+REM echo "Port already in use"
+REM ) else (
+REM set port = %1
+REM echo "Port not in use"
+REM)
