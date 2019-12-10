243681ff248ecfad846860b3f1eca016968c7b97
jlink-datev <jlink-datev@datev.de>
2019-12-10 12:00:09 +0100

    readme updated

--- docs/downloads/run-app.sh
+++ docs/downloads/run-app.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-port=$1
-
-if ( nc -zv localhost $port 2>&1 >/dev/null ); then
-  java -jar web/target/web-1.0-SNAPSHOT.jar --server.port=0
-else
-  java -jar web/target/web-1.0-SNAPSHOT.jar --server.port=$port
-fi
+java -jar ./web-1.0-SNAPSHOT.jar --server.port=8080
